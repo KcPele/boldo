fba7f3501e84e1fd48693054a1a0deaad4054ce1
Kc Pele <fidekg123@gmail.com>
2025-10-18 09:49:43 +0100

    feat: added heading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
   subsets: ["latin"],
 });
 
@@ -27,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`mx-auto max-w-[1200px] px-2  ${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`mx-auto max-w-[1200px] px-2  ${openSans.className} ${openSans.variable} antialiased`}
       >
         <Navbar />
         {children}
---
+++ src/components/heading.tsx
@@ -0,0 +1,8 @@
+export function Heading() {
+  return (
+    <div className="text-center mt-11 p-7">
+      <h3 className="text-boldo-blue-dark text-xl">Blog</h3>
+      <h3 className="text-boldo-blue-dark text-[64px]  ">Thoughts and words</h3>
+    </div>
+  );
+}
