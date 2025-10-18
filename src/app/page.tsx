c49458e83e8bb7a176f99f0fd4ac5f0b4b41e73d
Kc Pele <fidekg123@gmail.com>
2025-10-18 10:08:24 +0100

    feat: added seperator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Heading } from "@/components/heading";
 import { FeaturedArticle } from "@/components/featured-article";
+import { Separator } from "@radix-ui/react-separator";
 
 export default function Home() {
   return (
     <div>
       <Heading />
       <FeaturedArticle />
+      <Separator className="my-20 border-boldo-blue-dark border-[0.5px]" />
     </div>
   );
 }
